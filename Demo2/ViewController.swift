52b5f919c02c93e7719cb48b2e13e01404e560d5
KevinHung <kevinaugust145@gmail.com>
2024-07-23 13:10:32 +0800

    Initial Commit

---
+++ Demo2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo2
+//
+//  Created by 洪宗燦 on 2024/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Demo2Tests/Demo2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Demo2Tests.swift
+//  Demo2Tests
+//
+//  Created by 洪宗燦 on 2024/7/23.
+//
+
+import Testing
+@testable import Demo2
+
+struct Demo2Tests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
